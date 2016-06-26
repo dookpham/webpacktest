61c3844cb0c7e5495363b6d98f024276b5388741
Hack Reactor Students <accounts@hackreactor.com>
2016-06-25 17:42:13 -0700

    webpack tutorial

---
+++ hello.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import World from './world.jsx';
+ 
+export default class Hello extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+        <World />
+      </div>
+      )
+  }
+}
+ 
